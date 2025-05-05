5770056424dbe48f2a8376ae9a0a6cb7cfc7ce37
nestaleks <140888222+nestaleks@users.noreply.github.com>
2025-05-05 19:35:29 +0300

    qq

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -14,19 +14,21 @@ class BreadcrumbGenerator {
 
     getBreadcrumbData() {
         const pages = {
-            'index.html': { title: 'Головна', icon: './img/icons/home.svg' },
-            'dashboard.html': { title: 'Дешборд', icon: './img/icons/dashboard.svg', parent: 'index.html' },
-            'experts.html': { title: 'Експерти', icon: './img/icons/clients.svg', parent: 'index.html' },
-            'expert-page.html': { title: 'Антон Багінський', icon: './img/icons/clients.svg', parent: 'experts.html' },
-            'appointments.html': { title: 'Мої Зустрічі', icon: 'appointments', parent: 'index.html' },
-            'appointment.html': { title: 'Антон Багінський. Перша Консультація', icon: 'appointment', parent: 'appointments.html' },
-            'calendar.html': { title: 'Календар', icon: 'calendar', parent: 'index.html' },
-            'faq.html': { title: 'Техн-підтримка', icon: './img/icons/support-blue.svg', parent: 'index.html' },
-            'client-profile.html': { title: 'Профіль клієнта', icon: './img/icons/user.svg', parent: 'index.html' },
-            'expert-profile.html': { title: 'Профіль експерта', icon: './img/icons/user.svg', parent: 'index.html' },
-            'client-wallet.html': { title: 'Гаманець', icon: './img/icons/money.svg', parent: 'index.html' },
-            'expert-wallet.html': { title: 'Гаманець', icon: './img/icons/money.svg', parent: 'index.html' },
-            'payments-settings.html': { title: 'Налаштування Виплат', icon: './img/icons/banknotes.svg', parent: 'expert-wallet.html' }
+            "index.html": { title: "Головна", icon: "./img/icons/home.svg" },
+            "dashboard.html": { title: "Дешборд", icon: "./img/icons/dashboard.svg", parent: "index.html" },
+            "experts.html": { title: "Експерти", icon: "./img/icons/clients.svg", parent: "index.html" },
+            "expert-page.html": { title: "Антон Багінський", icon: "./img/icons/clients.svg", parent: "experts.html" },
+            "appointments.html": { title: "Мої Зустрічі", icon: "appointments", parent: "index.html" },
+            "appointment.html": { title: "Антон Багінський. Перша Консультація", icon: "appointment", parent: "appointments.html" },
+            "calendar.html": { title: "Календар", icon: "calendar", parent: "index.html" },
+            "faq.html": { title: "Техн-підтримка", icon: "./img/icons/support-blue.svg", parent: "index.html" },
+            "client-profile.html": { title: "Профіль клієнта", icon: "./img/icons/user.svg", parent: "index.html" },
+            "expert-profile.html": { title: "Профіль експерта", icon: "./img/icons/user.svg", parent: "index.html" },
+            "client-wallet.html": { title: "Гаманець", icon: "./img/icons/money.svg", parent: "index.html" },
+            "expert-wallet.html": { title: "Гаманець", icon: "./img/icons/money.svg", parent: "index.html" },
+            "payments-settings.html": { title: "Налаштування Виплат", icon: "./img/icons/banknotes.svg", parent: "expert-wallet.html" },
+            "clients.html": { title: "Клієнти", icon: "./img/icons/clients.svg", parent: "index.html" },
+            "client.html": { title: "Ім'я клієнта", icon: "./img/icons/clients.svg", parent: "index.html" }
         };
 
         return pages;
